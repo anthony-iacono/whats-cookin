6d53dbacdaf95aa988c9a5c4e734b388455da369
Tony Hsu <tdh425@gmail.com>
2021-08-31 15:49:40 -0600

    View a list of all recipes

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,13 @@ import recipeData from './data/recipes';
 const allRecipes = document.querySelector('.js-all-recipes');
 const recipeRepository = new RecipeRepository(recipeData);
 
-
+recipeRepository.recipes.forEach(recipe => {
+  const recipeCard = `<article class="recipe-card">
+  <h2>${recipe.name}</h2>
+  <img src=${recipe.image}>
+  </article>`
+  allRecipes.innerHTML += recipeCard
+});
 
 /*
 The goal is to view a list of all allRecipes
