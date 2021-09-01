308c4763d48ac528ab025c0806bdfcb465afbdab
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-31 19:58:10 -0700

    Finish displayRecipe and make helper functions SRP

--- src/scripts.js
+++ src/scripts.js
@@ -4,58 +4,64 @@ import RecipeRepository from './classes/RecipeRepository';
 import recipeData from './data/recipes';
 
 const allRecipes = document.querySelector('.js-all-recipes');
-const recipeRepository = new RecipeRepository(recipeData);
 const container = document.querySelector('.js-container');
 const recipePopout = document.querySelector('.js-recipe-popout');
+const recipeRepository = new RecipeRepository(recipeData);
 
+window.onload = displayAllRecipes();
 allRecipes.addEventListener('click', displayRecipe)
 
 function displayRecipe(event) {
-  console.log(event.target, "target")
-  const clickedRecipeId = parseInt(event.target.parentNode.id)
-  console.log(typeof clickedRecipeId)
-  const selectedRecipe = recipeRepository.recipes.find(recipe => recipe.id === clickedRecipeId)
   hide(allRecipes);
   show(recipePopout);
-  const instructionItems = selectedRecipe.instructions.forEach(instruction => {
-    `
-    <li>${instruction.instruction}</li>
-    `
-    })
-  recipePopout.innerHTML = `
-  <article>
-  <h2>${selectedRecipe.name}</h2>
-  <img src="${selectedRecipe.image}">
-  <ol class="instructions">
-  ${instructionItems}
-  </ol>
-  <p>${selectedRecipe.ingredients}</p>
-  </article>
-  `
-
+  const clickedRecipeId = parseInt(event.target.parentNode.id)
+  const selectedRecipe = recipeRepository.recipes.find(recipe => recipe.id === clickedRecipeId)
+  fillPopout(selectedRecipe);
+  displayPopoutIngredients(selectedRecipe);
+  displayPopoutInstructions(selectedRecipe);
 };
 
-recipeRepository.recipes.forEach(recipe => {
-  const recipeCard = `<article class="recipe-card" id="${recipe.id}">
-  <h2>${recipe.name}</h2>
-  <img src="${recipe.image}">
-  </article>`
-  allRecipes.innerHTML += recipeCard
-});
+function fillPopout(selectedRecipe) {
+  recipePopout.innerHTML = `
+    <article>
+      <h2>${selectedRecipe.name}</h2>
+      <img src="${selectedRecipe.image}">
+      <h3>Ingredients</h3>
+      <ul class="js-popout-ingredients"></ul>
+      <h3>Directions</h3>
+      <ol class="js-popout-instructions"></ol>
+      <h3>Cost</h3>
+      <p class="js-cost">${selectedRecipe.cost}</p>
+    </article>
+  `
+}
+
+function displayPopoutIngredients(selectedRecipe) {
+  selectedRecipe.ingredients.forEach(ingredient => {
+    document.querySelector('.js-popout-ingredients').innerHTML += `<li>${ingredient.amount} ${ingredient.unit} ${ingredient.name}</li>`;
+  });
+}
+
+function displayPopoutInstructions(selectedRecipe) {
+  selectedRecipe.instructions.forEach(instruction => {
+    document.querySelector('.js-popout-instructions').innerHTML += `<li>${instruction.instruction}</li>`;
+  });
+}
+
+function displayAllRecipes() {
+  recipeRepository.recipes.forEach(recipe => {
+    const recipeCard = `<article class="recipe-card" id="${recipe.id}">
+    <h2>${recipe.name}</h2>
+    <img src="${recipe.image}">
+    </article>`;
+    allRecipes.innerHTML += recipeCard;
+  });
+}
 
 function show(element) {
-  element.classList.remove('hidden')
-};
+  element.classList.remove('hidden');
+}
 
 function hide(element) {
-  element.classList.add('hidden')
-};
-/*
-The goal is to view a list of all allRecipes
-HOW
-we want to grab the recipes from the source -- import data file
-and we want to display them in some sort of order
-*/
-
-
-console.log(recipeRepository);
+  element.classList.add('hidden');
+}
