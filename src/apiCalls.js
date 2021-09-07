e86b59df90994dec18ab0f6aff00f74fb970969b
Tony Hsu <tdh425@gmail.com>
2021-09-07 13:27:46 -0600

    Add calls to fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,15 @@
-// Your fetch requests will live here!
+let userData;
+let ingredients;
+let recipes;
 
+fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+  .then((response) => response.json())
+  .then((data) => userData = data.userData)
 
-console.log('I will be a fetch request!')
+fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
+  .then((response) => response.json())
+  .then((data) => ingredients = data.ingredientsData)
+
+fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+  .then((response) => response.json())
+  .then((data) => recipes = data.recipeData)
