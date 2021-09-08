a6355d2ce032805baf2d229fddcaecf51262ebf4
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-07 20:39:59 -0700

    Remove comments and whitespace, and alphabetized scripts.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,27 +2,24 @@ function fetchUsers() {
   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
     .then((response) => response.json())
     .then((data) => data.usersData)
-    .catch(error => console.log("error"))
+    .catch(error => console.error(error))
 }
 
 function fetchIngredients() {
   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
     .then((response) => response.json())
     .then((data) => data.ingredientsData)
-    .catch(error => console.log("error"))
+    .catch(error => console.error(error))
 }
 
 function fetchRecipes() {
-   return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
     .then((response) => response.json())
     .then((data) => data.recipeData)
-    .catch(error => console.log("error"))
+    .catch(error => console.error(error))
 }
 
 export {
-  // userData,
-  // ingredientsData,
-  // recipeData,
   fetchUsers,
   fetchIngredients,
   fetchRecipes
