d0570c842d65d5aedcb9faa55c77c6278ed8e9db
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-15 15:54:56 -0700

    Add test for function and instance

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,11 +1,18 @@
 import { expect } from 'chai';
 
 describe('Pantry', function () {
-
+  let pantry = new Pantry();
+  
   beforeEach(function() {
 
   })
 
+  it('should be a function', function() {
+    expect(Pantry).to.be.a('function');
+  })
 
+  it('should be an instance of Pantry', function() {
+    expect(pantry).to.be.an.instanceof(Pantry);
+  })
 
 })
