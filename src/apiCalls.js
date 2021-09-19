7daf34722303e08d0a6e80f6762c498d8abf393a
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-09-19 15:31:41 -0700

    Update fetch paths

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,23 +1,21 @@
 function fetchUsers() {
-  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+  return fetch("http://localhost:3001/api/v1/users")
     .then((response) => response.json())
-    .then((data) => data.usersData)
     .catch(error => console.error(error))
 }
 
 function fetchRecipes() {
-  return fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+  return fetch("http://localhost:3001/api/v1/recipes")
     .then((response) => response.json())
-    .then((data) => data.recipeData)
     .catch(error => console.error(error))
 }
 
 function fetchIngredients() {
-  return fetch( "https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients" )
+  return fetch("http://localhost:3001/api/v1/ingredients")
     .then((response) => response.json())
-    .then((data) => data.ingredientsData)
     .catch(error => console.error(error))
 }
+
 export {
   fetchUsers,
   fetchRecipes,
