0f4e2a02e9168e107767f4f2d7523239072e9174
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-11 17:32:50 -0800

    Update fetch URLs to deployed API server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,18 @@
 
 function fetchUsers() {
-  return fetch("http://localhost:3001/api/v1/users")
+  return fetch("https://whats-cookin-api-data.herokuapp.com/api/v1/users")
     .then((response) => response.json())
     .catch(error => console.error(error))
   }
 
 function fetchRecipes() {
-  return fetch("http://localhost:3001/api/v1/recipes")
+  return fetch("https://whats-cookin-api-data.herokuapp.com/api/v1/recipes")
     .then((response) => response.json())
     .catch(error => console.error(error))
 }
 
 function fetchIngredients() {
-  return fetch("http://localhost:3001/api/v1/ingredients")
+  return fetch("https://whats-cookin-api-data.herokuapp.com/api/v1/ingredients")
     .then((response) => response.json())
     .catch(error => console.error(error))
 }
