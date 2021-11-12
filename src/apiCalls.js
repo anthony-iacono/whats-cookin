af3bf1d2fb2423884d7448c3652abe3ab016d165
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-11-11 17:47:28 -0800

    Move index.html to root

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ function fetchUsers() {
   return fetch("https://whats-cookin-api-data.herokuapp.com/api/v1/users")
     .then((response) => response.json())
     .catch(error => console.error(error))
-  }
+}
 
 function fetchRecipes() {
   return fetch("https://whats-cookin-api-data.herokuapp.com/api/v1/recipes")
